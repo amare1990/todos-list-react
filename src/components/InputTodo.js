59d9f50ab02ab480184a0a55a201d0472f17435c
amare1990 <amaremek@gmail.com>
2022-12-08 20:02:04 +0300

    Add function-based InputTodo.js

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,43 +1,45 @@
-import React, { Component } from "react"
+import React, { useState } from 'react';
+import { FaPlusCircle } from 'react-icons/fa';
 
-class InputTodo extends Component {
-  state = {
-    title: "",
-  }
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,    
-    })
-  }
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
 
-  handleSubmit = e => {
-    e.preventDefault();
-    if(this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title);
-      this.setState({    
-        title: ""  
-      });
-    } else {
-      alert("Please input the item")
-    } 
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add todo..."
-          value={this.state.title}
-          name="title"          
-          onChange={this.onChange}
-        />
-        <button className="input-submit">Submit</button>
-      </form>
-    )
-  }
-}
-export default InputTodo
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputText.title.trim()) {
+      // props.addTodoProps(inputText.title);
+      setInputText({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
 
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">
+        <FaPlusCircle style={{ color: 'darkcyan', fontSize: '20px', marginTop: '2px' }} />
+      </button>
+    </form>
+  );
+};
 
+export default InputTodo;
